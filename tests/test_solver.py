bcfd65b6ff1704ed12638f266b63c290bf0be1ba
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2016-08-31 01:18:50 -0300

    minor fixes before sleeping. code is still non working

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -8,22 +8,27 @@ test_chess_configurations
 Tests for `chess_configurations` module.
 """
 
-import pytest
-
-from contextlib import contextmanager
-from click.testing import CliRunner
-
 from chess_configurations.solver import backtracking
+from chess_configurations.models import Board, Piece
 
 
 class TestSolverWithBoardCases(object):
-
-    @classmethod
-    def setup_class(cls):
-        pass
+    """
+        Board test cases for the solver
+    """
 
     def test_very_simple_1x1_board_with_one_piece(self):
+        """
+            A board with one position
+        """
         board = Board(1, 1)
         pieces = [Piece('Q')]
         for solutions in backtracking(board, pieces, 0, 0):
             pass
+
+    def test_a_board_were_only_one_piece_can_be_added(self):
+        """
+            In this case we will set a board of 2x2 with only queen.
+            There is no way to add more than one piece.
+        """
+        pass
